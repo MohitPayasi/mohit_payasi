5ffeb82d4da78819417ea536a3b5e22faab9b39a
MohitPayasi <itsmpayasi04@gmail.com>
2026-09-24 15:50:21 +0530

    abc

--- project/fast_api.py
+++ project/fast_api.py
@@ -2,7 +2,15 @@
 
 
 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+groq_api= os.getenv("GROQ_API")
+model = ChatGroq(model="openai/gpt-oss-20b",api_key=os.getenv("groq_api"))
 
 
 
